e87a4ccbc3f05eb1a5c7b8dba2d325771cac60b1
MorganEJLA <mojones311@gmail.com>
2026-04-09 14:20:35 -0400

    auditing code

--- api/anthropic.js
+++ api/anthropic.js
@@ -7,7 +7,10 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://chromaterra.vercel.app",
+  );
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
