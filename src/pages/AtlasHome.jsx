729183f2473efc4b456c12b9aea82f63b8c712ad
MorganEJLA <mojones311@gmail.com>
2026-04-08 21:42:14 -0400

    updated delete of country

--- src/pages/AtlasHome.jsx
+++ src/pages/AtlasHome.jsx
@@ -26,7 +26,70 @@ const ghostButtonStyle = {
   padding: 0,
 };
 
-function AlbumCard({ album, user, onClick, onDelete, onRename }) {
+function CountryEdit({ album, onRename }) {
+  const { editing, value, setValue, start, handleKeyDown } = useInlineEdit(
+    album.country,
+    onRename,
+  );
+
+  if (editing) {
+    return (
+      <div
+        onClick={(e) => e.stopPropagation()}
+        style={{ margin: "0 0 0.4rem 0" }}
+      >
+        <input
+          autoFocus
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          style={{
+            fontFamily: "'DM Mono', monospace",
+            fontSize: "0.55rem",
+            letterSpacing: "0.2em",
+            textTransform: "uppercase",
+            color: "#888",
+            border: "none",
+            borderBottom: "1px solid #888",
+            background: "transparent",
+            outline: "none",
+            width: "100%",
+          }}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <p
+      onClick={(e) => {
+        e.stopPropagation();
+        start(e);
+      }}
+      title="Click to edit"
+      style={{
+        fontFamily: "'DM Mono', monospace",
+        fontSize: "0.55rem",
+        letterSpacing: "0.2em",
+        textTransform: "uppercase",
+        color: "#888",
+        margin: "0 0 0.4rem 0",
+        cursor: "text",
+      }}
+    >
+      {album.country}
+    </p>
+  );
+}
+
+function AlbumCard({
+  album,
+  user,
+  onClick,
+  onDelete,
+  onRename,
+  onCountryRename,
+}) {
   const { editing, value, setValue, start, save, handleKeyDown } =
     useInlineEdit(album.name, onRename);
 
@@ -50,18 +113,22 @@ function AlbumCard({ album, user, onClick, onDelete, onRename }) {
         ))}
       </div>
       <div style={{ padding: "1.25rem" }}>
-        <p
-          style={{
-            fontFamily: "'DM Mono', monospace",
-            fontSize: "0.55rem",
-            letterSpacing: "0.2em",
-            textTransform: "uppercase",
-            color: "#888",
-            margin: "0 0 0.4rem 0",
-          }}
-        >
-          {album.country}
-        </p>
+        {user ? (
+          <CountryEdit album={album} onRename={onCountryRename} />
+        ) : (
+          <p
+            style={{
+              fontFamily: "'DM Mono', monospace",
+              fontSize: "0.55rem",
+              letterSpacing: "0.2em",
+              textTransform: "uppercase",
+              color: "#888",
+              margin: "0 0 0.4rem 0",
+            }}
+          >
+            {album.country}
+          </p>
+        )}
 
         {editing ? (
           <input
